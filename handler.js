a70d74907c88d7bb32de2898b036aa56c9b70b0c
𐌗˚🍁｡ 𝖣𝖾𝗏𝖥𝖾𝖽𝖾𝟣𝟥ㅤ⚽⋅ <fedexyz13@gmail.com>
2025-08-30 10:59:35 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -542,7 +542,7 @@ global.dfail = (type, m, usedPrefix, command, conn) => {
 
 let edadaleatoria = ['10', '28', '20', '40', '18', '21', '15', '11', '9', '17', '25'].getRandom()
 let user2 = m.pushName || 'Anónimo'
-let verifyaleatorio = ['registrar', 'reg', 'verificar', 'verify', 'register'].getRandom()
+let verifyaleatorio = ['registrar', 'reg', 'verificar'].getRandom()
 
 const msg = {
 rowner: `˚🌑｡ El comando *${comando}* solo puede ser usado por los creadores del bot.`, 
